b45576756dcfa5581dd07d4894e0352549ebfd94
dinosaurtirex <sneakybeaky18@gmail.com>
2022-12-01 10:26:41 +0300

    Updated server/server.py

--- server/server.py
+++ server/server.py
@@ -20,6 +20,7 @@ async def talking(request: Request) -> HTTPResponse:
 
 @app.route('/update', methods=["GET", "POST"])
 async def talking(request: Request) -> HTTPResponse:
+    print("users_in_chat", [[username, ip] for username, ip in zip(users.keys(),users.values())])
     return response.json({
         "status": actual_messages, 
         "users_in_chat": [[username, ip] for username, ip in zip(
