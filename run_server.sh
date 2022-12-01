0c0db8c584c4fc83beccd1e7253f0f19699230e5
dinosaurtirex <58666088+dinosaurtirex@users.noreply.github.com>
2022-12-01 09:50:57 +0300

    Create bash script to runserver
    Syntax: ./run_server.sh port
    ./run_server.sh 2121

---
+++ run_server.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+port=$1
+
+if [ -d venv ]; then
+  source venv/bin/activate
+  sanic server.app -H 0.0.0.0 -p $port
+else
+  python3.10 -m venv venv
+  source venv/bin/activate
+  pip install -r reqs.txt
+  sanic server.app -H 0.0.0.0 -p $port
+fi
