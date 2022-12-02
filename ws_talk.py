44feb8f7ac86d87a6eb75b0386343e47d5f4b35b
dinosaurtirex <sneakybeaky18@gmail.com>
2022-12-02 08:21:30 +0300

    Updated ws_talk.py

--- ws_talk.py
+++ ws_talk.py
@@ -2,7 +2,8 @@ import json
 from websocket import create_connection
 
 ws = create_connection("ws://localhost:1212/update")
-ws.send(payload="")#json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
-result =  ws.recv()
-print(result)
+while True:
+    ws.send(payload="")#json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
+    result =  ws.recv()
+    print(result)
 ws.close()
