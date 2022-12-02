da58d25ee2fa1c016a282ed7aee0cc0f8908a080
dinosaurtirex <sneakybeaky18@gmail.com>
2022-12-02 08:30:31 +0300

    Updated ws_talk.py

--- ws_talk.py
+++ ws_talk.py
@@ -3,8 +3,9 @@ from websocket import create_connection
 
 ws = create_connection("ws://localhost:1212/talk")
 while True:
-    ws.send(payload="{'text': 123, 'username': 'bob'}")#json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
+    ws.send(payload=b"{'text': 123, 'username': 'bob'}")#json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
     result =  ws.recv()
-    print(eval(result))
     print(result)
+    #print(eval(result))
+    #print(result)
 ws.close()
