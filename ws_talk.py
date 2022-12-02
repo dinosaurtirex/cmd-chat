eee9bbf8dc3d73bdedc61553efd388ac766fa8f5
dinosaurtirex <sneakybeaky18@gmail.com>
2022-12-02 08:17:47 +0300

    Updated ws_talk.py

--- ws_talk.py
+++ ws_talk.py
@@ -1,8 +1,8 @@
 import json
 from websocket import create_connection
 
-ws = create_connection("wss://ws.dogechain.info/inv")
-ws.send(json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
+ws = create_connection("ws://192.168.1.1/update")
+ws.send()#json.dumps({"op":"addr_sub", "addr":"dogecoin_address"}))
 result =  ws.recv()
-print (result)
+print(result)
 ws.close()
