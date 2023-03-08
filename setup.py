f1f8b2147964fd6cd947924038e1d3ea4cef095b
dinosaurtirex <sneakybeaky18@gmail.com>
2023-03-08 19:32:44 +0300

    Update pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
   
 setuptools.setup(
     name="cmd_chat",
-    version="1.3",
+    version="1.0",
     author="dinosaurtirex",
     author_email="sneakybeaky18@gmail.com",
     packages=[
